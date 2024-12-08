b20a13862f1ba6332378ac858af53a1784ba7d92
NagasonicDev <nagasonicdev@gmail.com>
2024-12-09 10:34:30 +1100

    Allowed setting of the value.

--- src/main/java/ca/nagasonic/skonic/elements/citizens/expressions/ExprNameOfCitizen.java
+++ src/main/java/ca/nagasonic/skonic/elements/citizens/expressions/ExprNameOfCitizen.java
@@ -1,14 +1,12 @@
 package ca.nagasonic.skonic.elements.citizens.expressions;
 
-import ch.njol.skript.Skript;
+import ch.njol.skript.classes.Changer;
 import ch.njol.skript.doc.*;
-import ch.njol.skript.lang.Expression;
-import ch.njol.skript.lang.ExpressionType;
-import ch.njol.skript.lang.SkriptParser;
-import ch.njol.skript.lang.util.SimpleExpression;
-import ch.njol.util.Kleenean;
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import ch.njol.util.coll.CollectionUtils;
 import net.citizensnpcs.api.npc.NPC;
 import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @Name("Citizen Name")
@@ -17,44 +15,39 @@ import org.jetbrains.annotations.Nullable;
 @Since("1.0.5")
 @Examples("")
 @DocumentationId("citizen.name")
-public class ExprNameOfCitizen extends SimpleExpression<String> {
-    static {
-        Skript.registerExpression(ExprNameOfCitizen.class,
-                String.class,
-                ExpressionType.COMBINED,
-                "[the] name of %npc%");
+public class ExprNameOfCitizen extends SimplePropertyExpression<NPC, String> {
+    @Override
+    public @Nullable String convert(NPC npc) {
+        return npc.getName();
     }
 
-    private Expression<NPC> npcExpr;
-
+    @SuppressWarnings("NullableProblems")
     @Override
-    protected @Nullable String[] get(Event e) {
-        NPC npc = npcExpr.getSingle(e);
-        if (npcExpr != null){
-            if (npc != null){
-                return new String[]{npc.getName()};
-            }else return null;
-        }else return null;
+    public @Nullable Class<?>[] acceptChange(Changer.ChangeMode mode) {
+        if (mode == Changer.ChangeMode.SET) return CollectionUtils.array(String.class);
+        return null;
+    }
+
+    @SuppressWarnings({"NullableProblems", "ConstantValue"})
+    @Override
+    public void change(Event event, @Nullable Object[] delta, Changer.ChangeMode mode) {
+        if (delta != null && delta[0] instanceof Boolean) {
+            String name = (String) delta[0];
+            for (NPC npc : getExpr().getArray(event)) {
+                npc.setName(name);
+            }
+        }
     }
 
     @Override
-    public boolean isSingle() {
-        return true;
-    }
-
-    @Override
+    @NotNull
     public Class<? extends String> getReturnType() {
         return String.class;
     }
 
     @Override
-    public String toString(@Nullable Event e, boolean debug) {
-        return "name of citizen with id " + npcExpr.toString(e, debug);
-    }
-
-    @Override
-    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
-        npcExpr = (Expression<NPC>) exprs[0];
-        return true;
+    @NotNull
+    protected String getPropertyName() {
+        return "citizen name";
     }
 }
