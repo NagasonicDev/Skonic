bde6b8f70f6b81380fdbf95f70fb0dcf88262f1a
NagasonicDev <nagasonicdev@gmail.com>
2024-12-09 10:35:36 +1100

    Added condition for whether a citizen can be attacked.

---
+++ src/main/java/ca/nagasonic/skonic/elements/citizens/conditions/CondCitizenIsVulnerable.java
@@ -0,0 +1,51 @@
+package ca.nagasonic.skonic.elements.citizens.conditions;
+
+import ca.nagasonic.skonic.Skonic;
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.*;
+import ch.njol.skript.lang.Condition;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import net.citizensnpcs.api.npc.NPC;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.logging.Level;
+
+@Name("Citizen Is Vulnerable")
+@Description("Returns whether the Citizen can be attacked or not.")
+@Examples("")
+@Since("1.1")
+@RequiredPlugins("Citizens")
+@DocumentationId("citizen.isvulnerable")
+public class CondCitizenIsVulnerable extends Condition {
+    static {
+        Skript.registerCondition(CondCitizenIsVulnerable.class, "%npc% is vulnerable");
+    }
+    private Expression<NPC> npcExpr;
+    @Override
+    public boolean check(Event event) {
+        if (npcExpr == null) {
+            Skonic.log(Level.SEVERE, "NPC is null");
+            return false;
+        }
+        NPC npc = npcExpr.getSingle(event);
+        if (npc == null) {
+            Skonic.log(Level.SEVERE, "NPC is null");
+            return false;
+        }
+        return npc.isProtected();
+    }
+
+    @Override
+    public String toString(@Nullable Event event, boolean b) {
+        return npcExpr.toString(event, b) + "is vulnerable";
+    }
+
+    @Override
+    public boolean init(Expression<?>[] exprs, int i, Kleenean kleenean, SkriptParser.ParseResult parseResult) {
+        npcExpr = (Expression<NPC>) exprs[0];
+        return true;
+    }
+}
