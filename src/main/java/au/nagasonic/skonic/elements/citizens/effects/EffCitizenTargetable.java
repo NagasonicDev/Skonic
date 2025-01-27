2567f7ce49606172c618038a0d10f8beed08b4cb
NagasonicDev <nagasonicdev@gmail.com>
2025-01-28 10:05:06 +1100

    Fix Scripts (Hopefully)

--- src/main/java/au/nagasonic/skonic/elements/citizens/effects/EffCitizenTargetable.java
+++ src/main/java/au/nagasonic/skonic/elements/citizens/effects/EffCitizenTargetable.java
@@ -8,7 +8,10 @@ import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.util.Kleenean;
 import net.citizensnpcs.api.npc.NPC;
+import net.citizensnpcs.api.trait.trait.MobType;
 import net.citizensnpcs.trait.TargetableTrait;
+import net.citizensnpcs.util.NMS;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
@@ -17,16 +20,17 @@ import java.util.logging.Level;
 @Name("Make Citizen Targetable")
 @Description({"Make a citizen targetable to other mobs.", "Or you can stop this process."})
 @Examples({"make npc with id 2 targetable", "make npc with id 3 not targetable"})
-@Since("1.2")
+@Since("1.2, INSERT VERSION (temporary)")
 @RequiredPlugins("Citizens")
 public class EffCitizenTargetable extends Effect {
     static {
         Skript.registerEffect(EffCitizenTargetable.class,
-                "make %npc% [[able to] be] target(ed|able)",
-                "make %npc% not [[able to] be] target(ed|able)");
+                "make %npc% [[able to] be] target(ed|able) [t:temporarily]",
+                "make %npc% not [[able to] be] target(ed|able) [t:temporarily]");
     }
     private int pattern;
     private Expression<NPC> npcExpr;
+    private boolean t;
     @Override
     protected void execute(Event event) {
         if (npcExpr != null){
@@ -34,10 +38,19 @@ public class EffCitizenTargetable extends Effect {
             if (npc != null){
                 if (pattern == 0){
                     npc.getOrAddTrait(TargetableTrait.class).setTargetable(true);
-                }else if (pattern == 1){
+                }else {
                     npc.getOrAddTrait(TargetableTrait.class).setTargetable(false);
-                }else{
-                    Skonic.log(Level.SEVERE, "Pattern was entered incorrectly.");
+                }
+                if (pattern == 0 && npc.getOrAddTrait(MobType.class).getType() == EntityType.PLAYER
+                        && npc.shouldRemoveFromPlayerList()) {
+                    if (t == true) {
+                        npc.data().set(NPC.Metadata.REMOVE_FROM_PLAYERLIST, false);
+                    } else {
+                        npc.data().setPersistent(NPC.Metadata.REMOVE_FROM_PLAYERLIST, false);
+                    }
+                    if (npc.isSpawned()) {
+                        NMS.addOrRemoveFromPlayerList(npc.getEntity(), false);
+                    }
                 }
             }else Skonic.log(Level.SEVERE, "NPC cannot be null");
         }else Skonic.log(Level.SEVERE, "NPC Expression cannot be null");
@@ -54,6 +67,7 @@ public class EffCitizenTargetable extends Effect {
     public boolean init(Expression<?>[] exprs, int i, Kleenean kleenean, SkriptParser.ParseResult parseResult) {
         pattern = i;
         npcExpr = (Expression<NPC>) exprs[0];
+        t = parseResult.hasTag("t");
         return true;
     }
 }
