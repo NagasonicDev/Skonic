82f768be5633df19253e4d1bd42a9b23467e29b6
NagasonicDev <nagasonicdev@gmail.com>
2026-01-29 18:00:06 +1100

    Add Citizen Metadata Elements

---
+++ src/main/java/au/nagasonic/skonic/elements/citizens/effects/EffCitizenMetadataModify.java
@@ -0,0 +1,88 @@
+package au.nagasonic.skonic.elements.citizens.effects;
+
+import au.nagasonic.skonic.Skonic;
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.util.AsyncEffect;
+import ch.njol.util.Kleenean;
+import net.citizensnpcs.api.npc.NPC;
+import org.bukkit.event.Event;
+import net.citizensnpcs.api.npc.NPC.Metadata;
+import org.jetbrains.annotations.Nullable;
+
+public class EffCitizenMetadataModify extends AsyncEffect {
+    static {
+        Skript.registerEffect(EffCitizenMetadataModify.class,
+                "(modify|set) [p:persistent] (citizen|npc) %npcmetadata% [metadata] of %npcs% to (%-boolean%|%number%)",
+                "(modify|set) %npcs%['s] [p:persistent] (citizen|npc) %npcmetadata% [metadata] to (%-boolean%|%number%)");
+    }
+    private Expression<NPC> npcExpr;
+    private Expression<Metadata> metadataExpr;
+    private Expression<Boolean> boolExpr;
+    private Expression<Number> numberExpr;
+    private int pattern;
+    private boolean p;
+
+    @Override
+    protected void execute(Event event) {
+        if (npcExpr == null || metadataExpr == null || pattern > 2) return;
+        if (boolExpr == null && numberExpr == null) return;
+        NPC[] npcs = npcExpr.getArray(event);
+        if (npcs == null) return;
+        Metadata metadata = metadataExpr.getSingle(event);
+        Object value = null;
+        Class<? extends Object> aclass = Object.class;
+        if (boolExpr != null){
+            value = boolExpr.getSingle(event);
+            aclass = Boolean.class;
+        } else if (numberExpr != null) {
+            Number num = numberExpr.getSingle(event);
+            Double dval = num.doubleValue();
+            Integer ival = num.intValue();
+            if (dval > ival){
+                value = dval;
+                aclass = Double.class;
+            }else {
+                value = ival;
+                aclass = Integer.class;
+            }
+        }
+        if (metadata == null || value == null) return;
+        if (metadata.accepts(aclass)){
+            for (NPC npc : npcs){
+                if (p){
+                    npc.data().setPersistent(metadata, value);
+                }else{
+                    npc.data().set(metadata, value);
+                }
+            }
+        }else{
+            Skonic.logger().severe("Metadata %metadata% does not accept %value% as a possible value.", metadata.toString(), value.getClass());
+        }
+    }
+
+    @Override
+    public String toString(@Nullable Event event, boolean b) {
+        return "";
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(Expression<?>[] exprs, int i, Kleenean kleenean, SkriptParser.ParseResult parseResult) {
+        this.pattern = i;
+        this.p = parseResult.hasTag("p");
+        if (pattern == 0){
+            this.npcExpr = (Expression<NPC>) exprs[1];
+            this.metadataExpr = (Expression<Metadata>) exprs[0];
+            this.boolExpr = (Expression<Boolean>) exprs[2];
+            this.numberExpr = (Expression<Number>) exprs[3];
+        }else{
+            this.npcExpr = (Expression<NPC>) exprs[0];
+            this.metadataExpr = (Expression<Metadata>) exprs[1];
+            this.boolExpr = (Expression<Boolean>) exprs[2];
+            this.numberExpr = (Expression<Number>) exprs[3];
+        }
+        return true;
+    }
+}
